2f3bd7f305087f6a3097142c11e97575cf8eafac
Gianni Gabriel <clondireanug@gmail.com>
2025-03-20 00:00:01 +0100

    added a product page when clicking on a porduct

--- src/components/shoppingCard.jsx
+++ src/components/shoppingCard.jsx
@@ -1,13 +1,22 @@
-import { apiKey } from "../config";
+import { apiKey, errorUrl } from "../config";
 import {useEffect, useState} from "react"
+import { useNavigate } from "react-router-dom";
 const options = {method: 'GET', headers: {Authorization: apiKey}};
 export const ShoppingCard= () => {
     const [error, setError] = useState(null)
     const [sneakers, setSneakers]=useState([])
     const [loading, setLoading] = useState(true)
+    const[productId, setProductId]= useState("")
+    const [productName, setProductName]=useState("")
     const [page, setPage]=useState(1) 
+
+    const navigate = useNavigate();
+        const handleClick=(id, name)=>{
+            navigate(  name.split(" ").join("-")+ "/dp/" +id);
+            
+        }
         const fetchSneakers = async(page)=>{
-            fetch(`https://api.sneakersapi.dev/api/v3/stockx/products?limit=20&page=${page}&product_type=sneakers`, options)
+            fetch(`https://api.sneakersapi.dev/api/v3/stockx/products?limit=30&page=${page}&product_type=sneakers`, options)
                 .then(response => {
                     if (response.status >= 400) {
                         throw new Error("server error");
@@ -17,7 +26,7 @@ export const ShoppingCard= () => {
                 // Filtramos solo los que tengan product_type === "sneakers" y tambien filtramos solo los datos que necesitemos para optimizar mas el codigo
                 .then(data => {
                     const filtredData=data.data
-                        .filter(item=>item.product_type==="sneakers")
+                        .filter(item=>item.product_type==="sneakers" && !item.image.includes(errorUrl))
                         .map(item => ({
                             id: item.id,
                             image: item.image,
@@ -35,7 +44,7 @@ export const ShoppingCard= () => {
         fetchSneakers(page)  
         
     },[page])
-    console.log(sneakers)
+
 
     if(error) return <p>The sneakers cant appear</p>
     if (loading) return <p>Loading...</p>
@@ -43,7 +52,7 @@ export const ShoppingCard= () => {
         <section style={{display:"flex", flexWrap:"wrap"}}>
             {
                 sneakers.map((sneaker)=>(
-                    <article key={sneaker.id}>
+                    <article key={sneaker.id} onClick={()=> handleClick(sneaker.id, sneaker.title)} >
                         <img style={{width:"300px", height:"200px"}} src={sneaker.image} alt={"Image of" + sneaker.title} />
                         <section>
                             <p>{sneaker.title}</p>
