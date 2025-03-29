d2560ae5280c1ba3919e990a2ed41907610eb818
Gianni Gabriel <clondireanug@gmail.com>
2025-03-29 18:55:12 +0100

    fixed error of iamges in homepage not showing on vercel

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -13,15 +13,15 @@ export const HomePage = () => {
     navigate(`/shop/${brand}`);
   };
   const logos = [
-    { url: "src/assets/logos/jordan.webp", brand: "Jordan" },
-    { url: "src/assets/logos/adidas.webp", brand: "adidas" },
-    { url: "src/assets/logos/new-balance.webp", brand: "New Balance" },
-    { url: "src/assets/logos/nike.webp", brand: "Nike" },
-    { url: "src/assets/logos/vans.webp", brand: "Vans" },
-    { url: "src/assets/logos/puma.webp", brand: "Puma" },
-    { url: "src/assets/logos/converse.webp", brand: "Converse" },
-    { url: "src/assets/logos/fila.webp", brand: "Fila" },
-    { url: "src/assets/logos/reebok.webp", brand: "Reebok" },
+    { url: "/src/assets/logos/jordan.webp", brand: "Jordan" },
+    { url: "/src/assets/logos/adidas.webp", brand: "adidas" },
+    { url: "/src/assets/logos/new-balance.webp", brand: "New Balance" },
+    { url: "/src/assets/logos/nike.webp", brand: "Nike" },
+    { url: "/src/assets/logos/vans.webp", brand: "Vans" },
+    { url: "/src/assets/logos/puma.webp", brand: "Puma" },
+    { url: "/src/assets/logos/converse.webp", brand: "Converse" },
+    { url: "/src/assets/logos/fila.webp", brand: "Fila" },
+    { url: "/src/assets/logos/reebok.webp", brand: "Reebok" },
   ];
   return (
     <section className="homePageContainer">
